a38405c9e6350e7574f4ea8e30ed6df5360b1ee0
Piotr Porembinski <102904996+PiotrPor@users.noreply.github.com>
2023-12-15 09:55:39 +0100

    Comments (2/2)

--- DFA_C++/DFA.cpp
+++ DFA_C++/DFA.cpp
@@ -13,18 +13,18 @@
 //Ten enumerator sluzy za alfabet. Ma w sobie 3 mozliwe symbole wejsciowe
 enum Symbole
 {
-    m1,
-    m2,
-    m5
+    m1, //symbolizuje znak "1" na wejsciu, ktory symbolizuje monete 1 PLN
+    m2, //symbolizuje znak "2" na wejsciu, ktory symbolizuje monete 2 PLN
+    m5  //symbolizuje znak "5" na wejsciu, ktory symbolizuje monete 5 PLN
 };
 
 //Ten enumerator przechowuje mozliwe stany (poprawne), w ktorych moze byc automat
 enum MozliweStany
 {
-    q0, // "do automatu wrzucono 0 PLN"
-    q1, // "do automatu wrzucono 1 PLN"
-    q2,
-    q3,
+    q0, //symoblizuje, ze do automatu wrzucono 0 PLN
+    q1, //symoblizuje, ze do automatu wrzucono 1 PLN
+    q2, // ... 2 PLN
+    q3, //... itd.
     q4,
     q5,
     q6,
@@ -36,15 +36,15 @@ enum MozliweStany
     q12,
     q13,
     q14,
-    q15, // "do automatu wrzucono 15 PLN"
+    q15, //symbolizuje, ze do automatu wrzucono 15 PLN
     q16,
     q17,
     q18,
-    q19  // "do automatu wrzucono 19 PLN"
+    q19  //symbolizuje, ze do automatu wrzucono 19 PLN
 };
 
 //
-// ========  KLASA ========
+// ========  KLASA (jej deklaracja) ========
 
 //obiekt tej klasy bedzie automatem deterministycznym
 class AutomatDeterministyczny
@@ -56,14 +56,13 @@ public:
     AutomatDeterministyczny(); //konstruktor bezparametrowy
     void funkcja_przejscia(Symbole s); //po otrzymaniu symbolu automat zmienia stan
     void przedstaw_zakonczenie(); // napisze "bardziej ludzki" opis sytuacji na koniec pracy automatu
-    void zresetuj_automat(); // potrzebne gdy nastapi blad
     MozliweStany daj_aktualny_stan(); // w jakim stanie katualnie jest automat
     bool czy_jest_w_stanie_akceptujacym();
     int ile_pieniedzy_zgromadzone();
 };
 
 //
-// ========  FUNKCJE ========
+// ========  FUNKCJE (ich deklaracje) ========
 
 bool czy_nalezy_do_alfabetu(char sym); //czy wczytany symbol nalezy do alfabetu ("1", "2" i "5")
 Symbole zamien_na_symbol(char sym); //symbol (jako 'char') zmieniony na symbol jako wartosc z enumeratora
@@ -122,32 +121,32 @@ int main()
 }
 
 //
-// ========  KLASA ========
+// ========  KLASA (definicje jej metod) ========
 
 AutomatDeterministyczny::AutomatDeterministyczny() //konstruktor bezparametrowy
 {
-	stan = stan_poczatkowy;
+	stan = stan_poczatkowy; //na poczatku automat jest w stanie akceptujacym
 }
 
-void AutomatDeterministyczny::funkcja_przejscia(Symbole s)
+void AutomatDeterministyczny::funkcja_przejscia(Symbole s) //dziala jak przechodzenie po tablicy przejsc
 {
-	switch (stan)
+	switch (stan) //podejmie decyzje na podstawie aktualnego stanu automatu
 	{
-	case MozliweStany::q0:
+	case MozliweStany::q0: //jest automat jest teraz w stanie q0
 	{
-		if (s == Symbole::m1) { stan = MozliweStany::q1; }
-		if (s == Symbole::m2) { stan = MozliweStany::q2; }
-		if (s == Symbole::m5) { stan = MozliweStany::q5; }
+		if (s == Symbole::m1) { stan = MozliweStany::q1; } //przechodzi w stan q1
+		if (s == Symbole::m2) { stan = MozliweStany::q2; } //przechodzi w stan q2
+		if (s == Symbole::m5) { stan = MozliweStany::q5; } //przechodzi w stan q5
 		break;
 	}
-	case MozliweStany::q1:
+	case MozliweStany::q1: //jesli jest teraz w stanie q1
 	{
-		if (s == Symbole::m1) { stan = MozliweStany::q2; }
+		if (s == Symbole::m1) { stan = MozliweStany::q2; } //przechodzi w stan...
 		if (s == Symbole::m2) { stan = MozliweStany::q3; }
 		if (s == Symbole::m5) { stan = MozliweStany::q6; }
 		break;
 	}
-	case MozliweStany::q2:
+	case MozliweStany::q2: //jesli jest teraz w stanie... itd
 	{
 		if (s == Symbole::m1) { stan = MozliweStany::q3; }
 		if (s == Symbole::m2) { stan = MozliweStany::q4; }
@@ -238,27 +237,27 @@ void AutomatDeterministyczny::funkcja_przejscia(Symbole s)
 		if (s == Symbole::m5) { stan = MozliweStany::q19; }
 		break;
 	}
-	case MozliweStany::q15:
+	case MozliweStany::q15: //jesli jest w stanie q15 (jeden ze stanow akceptujacych)
 	{
 		stan = MozliweStany::q15; //nie zmienia stanu
 		break;
 	}
-	case MozliweStany::q16:
+	case MozliweStany::q16: //jesli jest w stanie q16 (jeden ze stanow akceptujacych)
 	{
 		stan = MozliweStany::q16; //nie zmienia stanu
 		break;
 	}
-	case MozliweStany::q17:
+	case MozliweStany::q17: //stan q17 to tez stan akceptujacy
 	{
 		stan = MozliweStany::q17; //nie zmienia stanu
 		break;
 	}
-	case MozliweStany::q18:
+	case MozliweStany::q18: //to tez stan akceptujacy
 	{
 		stan = MozliweStany::q18; //nie zmienia stanu
 		break;
 	}
-	case MozliweStany::q19:
+	case MozliweStany::q19: // q19 to ostatni ze stanow akceptujacych
 	{
 		stan = MozliweStany::q19; //nie zmienia stanu
 		break;
@@ -267,113 +266,109 @@ void AutomatDeterministyczny::funkcja_przejscia(Symbole s)
 	return;
 }
 
-void AutomatDeterministyczny::przedstaw_zakonczenie()
+void AutomatDeterministyczny::przedstaw_zakonczenie() //opisuje tekstowo rezultat po zakonczeniu analizy ciagu przez automat
 {
 	switch (stan)
 	{
-		case MozliweStany::q15:
+		case MozliweStany::q15: //jesli stan akceptujacy, w ktorym zakonczyl, to q15 (wrzucono do niego 15 PLN)
 	    {
 		    std::cout << "Automat wydaje bilet." << std::endl;
 			break;
 		}
-		case MozliweStany::q16:
+		case MozliweStany::q16: //jesli stan akceptujacy, w ktorym zakonczyl, to q16 (wrzucono do niego 16 PLN)
 		{
 			std::cout << "Automat wydaje bilet i reszte 1 PLN." << std::endl;
 			break;
 		}
-		case MozliweStany::q17:
+		case MozliweStany::q17: //zakonczyl w stanie q17
 		{
 			std::cout << "Automat wydaje bilet i reszte 2 PLN." << std::endl;
 			break;
 		}
-		case MozliweStany::q18:
+		case MozliweStany::q18: //zakonczyl w stanie q17
 		{
 			std::cout << "Automat wydaje bilet i reszte 3 PLN." << std::endl;
 			break;
 		}
-		case MozliweStany::q19:
+		case MozliweStany::q19: //zakonczyl w stanie q17
 		{
 			std::cout << "Automat wydaje bilet i reszte 4 PLN." << std::endl;
 			break;
 		}
 		default:
 		{
-			break; //nic nie robi, bo przy niepoprawnym zakonczeniu nie trzeba opisywac zakonczenia
+			//nic nie robi, bo przy niepoprawnym zakonczeniu nie bedzie opisywane zakonczenie
+			// "main()" juz zajmie sie wypisaniem bledu
+			break;
 		}
 	}
 	return;
 }
 
-void AutomatDeterministyczny::zresetuj_automat()
-{
-	stan = stan_poczatkowy;
-	return;
-}
-
-MozliweStany AutomatDeterministyczny::daj_aktualny_stan()
+MozliweStany AutomatDeterministyczny::daj_aktualny_stan() //zwraca, w jakim stanie jest automat (metoda "getter")
 {
 	return this->stan;
 }
 
-bool AutomatDeterministyczny::czy_jest_w_stanie_akceptujacym()
+bool AutomatDeterministyczny::czy_jest_w_stanie_akceptujacym() //zwraca TRUE jesli automat jest w stanie akceptujacym, inaczej zwraca FALSE
 {
 	bool werdykt = false;
-	if (stan == MozliweStany::q15)
+	if (stan == MozliweStany::q15) //jest w stanie q15 (jeden ze stanow akceptujacych)
 	{
 		werdykt = true;
 	}
-	else if (stan == MozliweStany::q16)
+	else if (stan == MozliweStany::q16) //jest w stanie q15
 	{
 		werdykt = true;
 	}
-	else if (stan == MozliweStany::q17)
+	else if (stan == MozliweStany::q17) //jest w stanie q15
 	{
 		werdykt = true;
 	}
-	else if (stan == MozliweStany::q18)
+	else if (stan == MozliweStany::q18) //jest w stanie q15
 	{
 		werdykt = true;
 	}
-	else if (stan == MozliweStany::q19)
+	else if (stan == MozliweStany::q19) //jest w stanie q15
 	{
 		werdykt = true;
 	}
 	return werdykt;
 }
 
-int AutomatDeterministyczny::ile_pieniedzy_zgromadzone()
+int AutomatDeterministyczny::ile_pieniedzy_zgromadzone() //ile pieniedzy wrzucono juz do automatu (zwraca liczbe)
 {
 	int zgromadzona_suma = 0;
 	switch (stan)
 	{
 	case MozliweStany::q0:
 	{
-		zgromadzona_suma = 0;
+		zgromadzona_suma = 0; //stan q0 oznacza, ze w automacie jest teraz 0 PLN
 		break;
 	}
 	case MozliweStany::q1:
 	{
-		zgromadzona_suma = 1;
+		zgromadzona_suma = 1; //stan q1 oznacza, ze w automacie jest teraz 1 PLN
 		break;
 	}
 	case MozliweStany::q2:
 	{
-		zgromadzona_suma = 2;
+		zgromadzona_suma = 2; //stan q2 oznacza, ze w automacie jest teraz 2 PLN
 		break;
 	}
 	case MozliweStany::q3:
 	{
-		zgromadzona_suma = 3;
+		zgromadzona_suma = 3; //to oznacza, ze jest 3 PLN
 		break;
 	}
 	case MozliweStany::q4:
 	{
-		zgromadzona_suma = 4;
+		zgromadzona_suma = 4; //to oznacza, ze 4 PLN
 		break;
 	}
 	case MozliweStany::q5:
 	{
-		zgromadzona_suma = 5;
+		zgromadzona_suma = 5; //... itd.
 		break;
 	}
 	case MozliweStany::q6:
@@ -423,17 +418,17 @@ int AutomatDeterministyczny::ile_pieniedzy_zgromadzone()
 	}
 	case MozliweStany::q15:
 	{
-		zgromadzona_suma = 15;
+		zgromadzona_suma = 15; //w automacie jest juz 15 PLN
 		break;
 	}
 	case MozliweStany::q16:
 	{
-		zgromadzona_suma = 16;
+		zgromadzona_suma = 16; //w automacie jest juz 16 PLN
 		break;
 	}
 	case MozliweStany::q17:
 	{
-		zgromadzona_suma = 17;
+		zgromadzona_suma = 17; //... itd.
 		break;
 	}
 	case MozliweStany::q18:
@@ -451,35 +446,35 @@ int AutomatDeterministyczny::ile_pieniedzy_zgromadzone()
 }
 
 //
-// ========  FUNKCJE ========
+// ========  FUNKCJE (ich definicje) ========
 
-bool czy_nalezy_do_alfabetu(char sym)
+bool czy_nalezy_do_alfabetu(char sym) //czy podany znak drukowalny nalezy do alfabetu
 {
-    return (sym == '1' || sym == '2' || sym == '5');
+	return (sym == '1' || sym == '2' || sym == '5'); //zwraca wynik operacji logicznej OR
 }
 
-Symbole zamien_na_symbol(char sym)
+Symbole zamien_na_symbol(char sym) //symbol alfabetu jako 'char' bedzie zmieniony w odpowiednia wartosc enumeratora
 {
     Symbole symbol_alfabetu;
     if (sym == '1')
     {
-        symbol_alfabetu = Symbole::m1;
+        symbol_alfabetu = Symbole::m1; //"1" symoblizowane przez wartosc "m1" z enumeratora
     }
     else if (sym == '2')
     {
-        symbol_alfabetu = Symbole::m2;
+        symbol_alfabetu = Symbole::m2; //"2" symoblizowane przez wartosc "m2" z enumeratora
     }
     else
     {
-        symbol_alfabetu = Symbole::m5;
+        symbol_alfabetu = Symbole::m5; //"5" symoblizowane przez wartosc "m5" z enumeratora
     }
     return symbol_alfabetu;
 }
 
-std::string stan_jako_string(MozliweStany s)
+std::string stan_jako_string(MozliweStany s) //zwraca oznaczenie stanu jako tekst a nie wartosc enumeratora
 {
     std::string tekstowo;
-    if (s == MozliweStany::q0) { tekstowo = "q0"; }
+    if (s == MozliweStany::q0) { tekstowo = "q0"; } //na przyklad stan oznaczany jako "q0" powoduje zwracanie tekstu "q0"
     else if (s == MozliweStany::q1) { tekstowo = "q1"; }
     else if (s == MozliweStany::q2) { tekstowo = "q2"; }
     else if (s == MozliweStany::q3) { tekstowo = "q3"; }
