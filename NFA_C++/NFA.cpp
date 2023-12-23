20b2a1b4162c710680f9cd843d6626756a73a731
Piotr Porembinski <102904996+PiotrPor@users.noreply.github.com>
2023-12-23 14:45:47 +0100

    small imperfection corrected
    Switch() case didn't mention one of cases. This case wouldn't ever appear but some compilers see it as error.

--- NFA_C++/NFA.cpp
+++ NFA_C++/NFA.cpp
@@ -108,8 +108,6 @@ int main()
             std::cout << "Stany automatu na koniec: ";
             NFA.wylistuj_aktualny_stan();
             std::cout << "\n\n====\n====\n";
-            
-            //std::cout << ciag << std::endl;
         }
     }
     plik.close();
@@ -317,7 +315,7 @@ Stany AutomatNiedeterministyczny::nastepny_stan_wedlug_tablicy(Stany stan_teraz,
             else { nowy_stan = Stany::X; }
             break;
         }
-        //---------------------
+        //stany gdzie juz wczytano 3 identyczne symbole jeden po drugim
         case Stany::Q03: { nowy_stan = Stany::X; break; }
         case Stany::Q13: { nowy_stan = Stany::X; break; }
         case Stany::Q23: { nowy_stan = Stany::X; break; }
@@ -325,6 +323,7 @@ Stany AutomatNiedeterministyczny::nastepny_stan_wedlug_tablicy(Stany stan_teraz,
         case Stany::Qa3: { nowy_stan = Stany::X; break; }
         case Stany::Qb3: { nowy_stan = Stany::X; break; }
         case Stany::Qc3: { nowy_stan = Stany::X; break; }
+        default: { /*w innych sytuacjach nie robi nic*/ }
     }
     return nowy_stan;
 }
