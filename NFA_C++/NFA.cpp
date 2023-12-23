5ffe2ad7e74a8470c92aaaf24d398f1c6f6b3b7d
Piotr Porembinski <102904996+PiotrPor@users.noreply.github.com>
2023-12-23 22:31:24 +0100

    Corrected traverse tree and output
    Tree's branches, which ended with an accepting state, are now not-terminated and the accepting states aren't omitted in the final output/report.

--- NFA_C++/NFA.cpp
+++ NFA_C++/NFA.cpp
@@ -316,13 +316,13 @@ Stany AutomatNiedeterministyczny::nastepny_stan_wedlug_tablicy(Stany stan_teraz,
             break;
         }
         //stany gdzie juz wczytano 3 identyczne symbole jeden po drugim
-        case Stany::Q03: { nowy_stan = Stany::X; break; }
-        case Stany::Q13: { nowy_stan = Stany::X; break; }
-        case Stany::Q23: { nowy_stan = Stany::X; break; }
-        case Stany::Q33: { nowy_stan = Stany::X; break; }
-        case Stany::Qa3: { nowy_stan = Stany::X; break; }
-        case Stany::Qb3: { nowy_stan = Stany::X; break; }
-        case Stany::Qc3: { nowy_stan = Stany::X; break; }
+        case Stany::Q03: { nowy_stan = Stany::Q03; break; }
+        case Stany::Q13: { nowy_stan = Stany::Q13; break; }
+        case Stany::Q23: { nowy_stan = Stany::Q23; break; }
+        case Stany::Q33: { nowy_stan = Stany::Q33; break; }
+        case Stany::Qa3: { nowy_stan = Stany::Qa3; break; }
+        case Stany::Qb3: { nowy_stan = Stany::Qb3; break; }
+        case Stany::Qc3: { nowy_stan = Stany::Qc3; break; }
         default: { nowy_stan = Stany::X; }
     }
     return nowy_stan;
