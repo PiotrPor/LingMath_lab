b86636005408ca626688ec2ba9e1daab8f6f1c47
Piotr Porembinski <102904996+PiotrPor@users.noreply.github.com>
2023-12-30 17:38:09 +0100

    Code implementation finished
    Code is finished
    A little bit of more testing may be needed (or not).
    Has no comments and no clean-up.

--- TuringMachine_C++/Turing.cpp
+++ TuringMachine_C++/Turing.cpp
@@ -17,10 +17,12 @@ enum Ruch
 enum Stany
 {
     Qs,
+    Q0,
     Q1,
     Q2,
+    Q3,
     Qe,
-    nic
+    pusty
 };
 
 const char Alfabet[] = {'0','1','o'};
@@ -56,7 +58,10 @@ int main()
     std::string liczba_binarna = "o1001";
     std::string zwiekszona_liczba;
     MaszynaTuringa MT;
+    //
+    std::cout << "Tasma przed: " << liczba_binarna << std::endl;
     zwiekszona_liczba = MT.dodaj_trzy(liczba_binarna);
+    std::cout << "Tasma po: " << zwiekszona_liczba << std::endl;
     return 0;
 }
 
@@ -77,24 +82,37 @@ std::string MaszynaTuringa::dodaj_trzy(std::string liczba)
     while (x >= 0)
     {
         co_zrobi = this->podejmij_decyzje(zwiekszona[x]);
-        if (co_zrobi.nowy_stan != Stany::nic)
+        //stan
+        if (co_zrobi.nowy_stan != Stany::pusty)
         {
             this->aktualny_stan = co_zrobi.nowy_stan;
         }
-        //
-        if (x > 0)
+        //symbol wpisany na tasme
+        if (co_zrobi.wpisywany_symbol != nic_nie_pisz)
         {
-            if (co_zrobi.wpisywany_symbol != nic_nie_pisz)
-            {
-                zwiekszona[x] = co_zrobi.wpisywany_symbol;
-            }
-            if (co_zrobi.ruch == Ruch::R) { x++; }
-            else if (co_zrobi.ruch == Ruch::L) { x--; }
-            else { x = x; }
+            zwiekszona[x] = co_zrobi.wpisywany_symbol;
         }
-        else //jest na koncu, x==0
+        //ruch
+        if (x == 0)
         {
-            //
+            if (zwiekszona[x] != Alfabet[2]) //jesli to nie symbol konca tasmy
+            {
+                zwiekszona.insert(0, "o");
+                x++;
+            }
+        }
+        if (co_zrobi.ruch != Ruch::nic)
+        {
+            if (co_zrobi.ruch == Ruch::R) { x++; }
+            else { x--; }
+        }
+        //zakoncz petle
+        if (co_zrobi.nowy_stan == Stany::pusty &&
+            co_zrobi.wpisywany_symbol == nic_nie_pisz && 
+            co_zrobi.ruch == Ruch::nic && 
+            zwiekszona[x] == Alfabet[2])
+        {
+            break;
         }
     }
     return zwiekszona;
@@ -116,31 +134,49 @@ Decyzja MaszynaTuringa::podejmij_decyzje(char ss)
     {
         case Stany::Qs:
         {
-            if (ss == '0') {}
-            else if (ss == '1') {}
-            else {}
+            if (ss == Alfabet[0]) { co_robic = {Stany::Q0, Alfabet[1], Ruch::L}; }
+            else if (ss == Alfabet[1]) { co_robic = { Stany::Q1, Alfabet[0], Ruch::L }; }
+            else { co_robic = { Stany::Q0, Alfabet[1], Ruch::L }; }
+            break;
+        }
+        case Stany::Q0:
+        {
+            if (ss == Alfabet[0]) { co_robic = { Stany::Q2, Alfabet[1], Ruch::L }; }
+            else if (ss == Alfabet[1]) { co_robic = { Stany::Q3, Alfabet[0], Ruch::L }; }
+            else { co_robic = { Stany::Q2, Alfabet[1], Ruch::L }; }
+            break;
         }
         case Stany::Q1:
         {
-            if (ss == '0') {}
-            else if (ss == '1') {}
-            else {}
+            if (ss == Alfabet[0]) { co_robic = { Stany::Q3, nic_nie_pisz, Ruch::L }; }
+            else if (ss == Alfabet[1]) { co_robic = { Stany::Q3, nic_nie_pisz, Ruch::L }; }
+            else { co_robic = { Stany::Q3, Alfabet[0], Ruch::L }; }
+            break;
         }
         case Stany::Q2:
         {
-            if (ss == '0') {}
-            else if (ss == '1') {}
-            else {}
+            if (ss == Alfabet[0]) { co_robic = { Stany::pusty, nic_nie_pisz, Ruch::L }; }
+            else if (ss == Alfabet[1]) { co_robic = { Stany::pusty, nic_nie_pisz, Ruch::L }; }
+            else { co_robic = { Stany::pusty, nic_nie_pisz, Ruch::nic }; }
+            break;
         }
-        case Stany::Qe:
+        case Stany::Q3: //todo
         {
-            if (ss == '0') {}
-            else if (ss == '1') {}
-            else {}
+            if (ss == Alfabet[0]) { co_robic = { Stany::Q2, Alfabet[1], Ruch::L }; }
+            else if (ss == Alfabet[1]) { co_robic = { Stany::pusty, Alfabet[0], Ruch::L }; }
+            else { co_robic = { Stany::Q2, Alfabet[1], Ruch::nic }; }
+            break;
+        }
+        case Stany::Qe: //todo
+        {
+            if (ss == Alfabet[0]) { co_robic = { Stany::pusty, nic_nie_pisz, Ruch::nic }; }
+            else if (ss == Alfabet[1]) { co_robic = { Stany::pusty, nic_nie_pisz, Ruch::nic }; }
+            else { co_robic = { Stany::pusty, nic_nie_pisz, Ruch::nic }; }
+            break;
         }
         default:
         {
-            co_robic = { Stany::nic, nic_nie_pisz, Ruch::nic };
+            co_robic = { Stany::pusty, nic_nie_pisz, Ruch::nic };
             break;
         }
     }
