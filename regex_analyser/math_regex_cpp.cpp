fbd3bb93e6d410f7100bbc209b1cbec5669dd57d
Piotr Porembinski <102904996+PiotrPor@users.noreply.github.com>
2024-01-19 00:25:26 +0100

    regex done up to S

--- regex_analyser/math_regex_cpp.cpp
+++ regex_analyser/math_regex_cpp.cpp
@@ -1,6 +1,3 @@
-// math_regex_cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
 #include <regex>
@@ -10,26 +7,29 @@ int main()
     std::string testowany = "25.02";
     std::string testowany2 = "4+20*0.25";
     std::string testowany3 = "(2+0.1)";
+    std::string testowany4 = "2.4*(5+4/2)";
     //
     std::string L = "\\-?[0-9]+(\\.[0-9]+)?";
     std::string P = "(\\+|\\-|\\*|\\/)";
 
     std::string W1 = L;
-    std::string W2 = "\\(" + L + P + L + "(" + P + L + ")*\\)";
+    std::string W2 = "\\(" + L + "(" + P + L + ")+\\)";
+    std::string W = "("+ W1 + "|" + W2 + ")";
+    std::string S = W + "(" + P + W + ")+";
 
-    std::string wzorzec_bez_nawiasu = "\\-?[0-9]+(\\.[0-9]+)?((\\+|\\-|\\*|\\/)\\-?[0-9]+(\\.[0-9]+)?)*";
-    std::string bez_nawiasu_2 = L + "(" + P + L + ")*";
-    std::string W_mozliwy_nawias = W1 + "|" + W2;
+    //std::string wzorzec_bez_nawiasu = "\\-?[0-9]+(\\.[0-9]+)?((\\+|\\-|\\*|\\/)\\-?[0-9]+(\\.[0-9]+)?)*";
+    //std::string bez_nawiasu_2 = L + "(" + P + L + ")*";
     //
-    std::cout << testowany3 << std::endl;
-    std::regex wyrazenie_regularne(W_mozliwy_nawias);
-    if (std::regex_match(testowany, wyrazenie_regularne))
+    std::cout << testowany4 << std::endl;
+    std::regex wyrazenie_regularne(S);
+    if (std::regex_match(testowany4, wyrazenie_regularne))
     {
-        std::cout << "Pasuje";
+        std::cout << "Pasuje" << std::endl;
     }
     else
     {
-        std::cout << "Nie pasuje do wzorca";
+        std::cout << "Nie pasuje do wzorca" << std::endl;
     }
+    std::cout << "\n---\n----\n" << S << "\n---\n----\n";
     return 0;
 }
