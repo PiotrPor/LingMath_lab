36118c469f91968ec744130cd8071374a1511474
Piotr Porembinski <102904996+PiotrPor@users.noreply.github.com>
2024-01-19 11:33:59 +0100

    many expressions separated with semi-colon

--- regex_analyser/math_regex_cpp.cpp
+++ regex_analyser/math_regex_cpp.cpp
@@ -8,6 +8,7 @@ int main()
     std::string testowany2 = "4+20*0.25";
     std::string testowany3 = "(2+0.1)";
     std::string testowany4 = "2.4*(5+4/2)";
+    std::string testowany5 = "5*(14-2.7);(22.8-1)/2";
     //
     std::string L = "\\-?[0-9]+(\\.[0-9]+)?";
     std::string P = "(\\+|\\-|\\*|\\/)";
@@ -16,13 +17,14 @@ int main()
     std::string W2 = "\\(" + L + "(" + P + L + ")+\\)";
     std::string W = "("+ W1 + "|" + W2 + ")";
     std::string S = W + "(" + P + W + ")+";
+    std::string Y = S + "(\\;" + S + ")*";
 
     //std::string wzorzec_bez_nawiasu = "\\-?[0-9]+(\\.[0-9]+)?((\\+|\\-|\\*|\\/)\\-?[0-9]+(\\.[0-9]+)?)*";
     //std::string bez_nawiasu_2 = L + "(" + P + L + ")*";
     //
-    std::cout << testowany4 << std::endl;
-    std::regex wyrazenie_regularne(S);
-    if (std::regex_match(testowany4, wyrazenie_regularne))
+    std::cout << testowany5 << std::endl;
+    std::regex wyrazenie_regularne(Y);
+    if (std::regex_match(testowany5, wyrazenie_regularne))
     {
         std::cout << "Pasuje" << std::endl;
     }
