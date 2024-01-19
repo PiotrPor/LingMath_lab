f166d16ce44b5f95bdfbc3d9595688c9cc0392f0
Piotr Porembinski <102904996+PiotrPor@users.noreply.github.com>
2024-01-19 23:10:49 +0100

    comments added

--- regex_analyser/math_regex_cpp.cpp
+++ regex_analyser/math_regex_cpp.cpp
@@ -2,24 +2,42 @@
 #include <string>
 #include <regex>
 
+/*
+  GRAMATYKA
+Y ::= S{;S}
+S ::= WPW{PW}
+W ::= L | (LPL{PL})
+L ::= C{C}D | -C{C}D
+D ::= .C{C} | ?
+C ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+P ::= + | - | * | /
+*/
+
+/*
+(\-?[0-9]+(\.[0-9]+)?|\(\-?[0-9]+(\.[0-9]+)?((\+|\-|\*|\/)\-?[0-9]+(\.[0-9]+)?)+\))((\+|\-|\*|\/)(\-?[0-9]+(\.[0-9]+)?|\(\-?[0-9]+(\.[0-9]+)?((\+|\-|\*|\/)\-?[0-9]+(\.[0-9]+)?)+\)))+(\;(\-?[0-9]+(\.[0-9]+)?|\(\-?[0-9]+(\.[0-9]+)?((\+|\-|\*|\/)\-?[0-9]+(\.[0-9]+)?)+\))((\+|\-|\*|\/)(\-?[0-9]+(\.[0-9]+)?|\(\-?[0-9]+(\.[0-9]+)?((\+|\-|\*|\/)\-?[0-9]+(\.[0-9]+)?)+\)))+)*
+*/
+
 int main()
 {
-    std::string testowany;
+    std::string testowany; //tutaj uzytkownik wpisze dzialanie/a
 
-    std::string L = "\\-?[0-9]+(\\.[0-9]+)?";
+    //tlumaczenie produkcji z gramatyki na wyrazenia regularne i laczenie w jeden wielki rgex
     std::string P = "(\\+|\\-|\\*|\\/)";
-    std::string W1 = L;
-    std::string W2 = "\\(" + L + "(" + P + L + ")+\\)";
+    std::string C = "[0-9]";
+    std::string D = "(\\." + C + "+)?";
+    std::string L = "\\-?" + C + "+" + D;
+    std::string W1 = L; //pierwsza polowa produkcji "W"
+    std::string W2 = "\\(" + L + "(" + P + L + ")+\\)"; //druga polowa produkcji "W"
     std::string W = "("+ W1 + "|" + W2 + ")";
     std::string S = W + "(" + P + W + ")+";
     std::string Y = S + "(\\;" + S + ")*";
 
     std::cout << "Wpisz dzialania na liczbach (jesli kilka to rozdziel je srednikami, bez odstepow):" << std::endl;
-    std::getline(std::cin, testowany);
+    std::getline(std::cin, testowany); //to co wpisze uzytkownik jest zapisane do string'a
 
-    std::cout << "\nCzy \""<< testowany <<"\" pasuje do wzorca?" << std::endl;
-    std::regex wyrazenie_regularne(Y);
-    if (std::regex_match(testowany, wyrazenie_regularne))
+    std::cout << "\nCzy \""<< testowany <<"\" pasuje do wzorca?" << std::endl; //wypisze to co podal uzytkownik
+    std::regex wyrazenie_regularne(Y); //tekstowo zapisane wyrazenie regularne bedzie teraz rozumiane przez program
+    if (std::regex_match(testowany, wyrazenie_regularne)) //czy podany string pasuje do wyrazenia regularnego
     {
         std::cout << "Pasuje" << std::endl;
     }
@@ -27,6 +45,6 @@ int main()
     {
         std::cout << "Nie pasuje" << std::endl;
     }
-    std::cout << "\n---\nwyrazenie regularne:\n" << Y << "\n----\n";
+    std::cout << "\n---\nwyrazenie regularne:\n" << Y << "\n----\n"; //wupisze wyrazenie regularne
     return 0;
 }
