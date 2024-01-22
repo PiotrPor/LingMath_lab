ca3227ffecd12377be051b1d87c30795c1578996
Piotr Porembinski <102904996+PiotrPor@users.noreply.github.com>
2024-01-22 23:07:42 +0100

    hardcoded full regex
    Regex is not built in the code step-by-step from the syntax rules.
    It's already hard-coded in its entirety

--- regex_analyser/math_regex_cpp.cpp
+++ regex_analyser/math_regex_cpp.cpp
@@ -14,29 +14,20 @@ P ::= + | - | * | /
 */
 
 /*
-(\-?[0-9]+(\.[0-9]+)?|\(\-?[0-9]+(\.[0-9]+)?((\+|\-|\*|\/)\-?[0-9]+(\.[0-9]+)?)+\))((\+|\-|\*|\/)(\-?[0-9]+(\.[0-9]+)?|\(\-?[0-9]+(\.[0-9]+)?((\+|\-|\*|\/)\-?[0-9]+(\.[0-9]+)?)+\)))+(\;(\-?[0-9]+(\.[0-9]+)?|\(\-?[0-9]+(\.[0-9]+)?((\+|\-|\*|\/)\-?[0-9]+(\.[0-9]+)?)+\))((\+|\-|\*|\/)(\-?[0-9]+(\.[0-9]+)?|\(\-?[0-9]+(\.[0-9]+)?((\+|\-|\*|\/)\-?[0-9]+(\.[0-9]+)?)+\)))+)*
+(\\-?[0-9]+(\\.[0-9]+)?|\\(\\-?[0-9]+(\\.[0-9]+)?([\\+\\-\\*\\/]\\-?[0-9]+(\\.[0-9]+)?)+\\))([\\+\\-\\*\\/](\\-?[0-9]+(\\.[0-9]+)?|\\(\\-?[0-9]+(\\.[0-9]+)?([\\+\\-\\*\\/]\\-?[0-9]+(\\.[0-9]+)?)+\\)))+(\\;(\\-?[0-9]+(\\.[0-9]+)?|\\(\\-?[0-9]+(\\.[0-9]+)?([\\+\\-\\*\\/]\\-?[0-9]+(\\.[0-9]+)?)+\\))([\\+\\-\\*\\/](\\-?[0-9]+(\\.[0-9]+)?|\\(\\-?[0-9]+(\\.[0-9]+)?([\\+\\-\\*\\/]\\-?[0-9]+(\\.[0-9]+)?)+\\)))+)*
 */
 
 int main()
 {
     std::string testowany; //tutaj uzytkownik wpisze dzialanie/a
-
-    //tlumaczenie produkcji z gramatyki na wyrazenia regularne i laczenie w jeden wielki rgex
-    std::string P = "(\\+|\\-|\\*|\\/)";
-    std::string C = "[0-9]";
-    std::string D = "(\\." + C + "+)?";
-    std::string L = "\\-?" + C + "+" + D;
-    std::string W1 = L; //pierwsza polowa produkcji "W"
-    std::string W2 = "\\(" + L + "(" + P + L + ")+\\)"; //druga polowa produkcji "W"
-    std::string W = "("+ W1 + "|" + W2 + ")";
-    std::string S = W + "(" + P + W + ")+";
-    std::string Y = S + "(\\;" + S + ")*";
+    //moje wyrazenie regularne
+    std::string regex_tekst = "(\\-?[0-9]+(\\.[0-9]+)?|\\(\\-?[0-9]+(\\.[0-9]+)?([\\+\\-\\*\\/]\\-?[0-9]+(\\.[0-9]+)?)+\\))([\\+\\-\\*\\/](\\-?[0-9]+(\\.[0-9]+)?|\\(\\-?[0-9]+(\\.[0-9]+)?([\\+\\-\\*\\/]\\-?[0-9]+(\\.[0-9]+)?)+\\)))+(\\;(\\-?[0-9]+(\\.[0-9]+)?|\\(\\-?[0-9]+(\\.[0-9]+)?([\\+\\-\\*\\/]\\-?[0-9]+(\\.[0-9]+)?)+\\))([\\+\\-\\*\\/](\\-?[0-9]+(\\.[0-9]+)?|\\(\\-?[0-9]+(\\.[0-9]+)?([\\+\\-\\*\\/]\\-?[0-9]+(\\.[0-9]+)?)+\\)))+)*";
 
     std::cout << "Wpisz dzialania na liczbach (jesli kilka to rozdziel je srednikami, bez odstepow):" << std::endl;
     std::getline(std::cin, testowany); //to co wpisze uzytkownik jest zapisane do string'a
 
     std::cout << "\nCzy \""<< testowany <<"\" pasuje do wzorca?" << std::endl; //wypisze to co podal uzytkownik
-    std::regex wyrazenie_regularne(Y); //tekstowo zapisane wyrazenie regularne bedzie teraz rozumiane przez program
+    std::regex wyrazenie_regularne(regex_tekst); //tekstowo zapisane wyrazenie regularne bedzie teraz rozumiane przez program
     if (std::regex_match(testowany, wyrazenie_regularne)) //czy podany string pasuje do wyrazenia regularnego
     {
         std::cout << "Pasuje" << std::endl;
@@ -45,6 +36,6 @@ int main()
     {
         std::cout << "Nie pasuje" << std::endl;
     }
-    std::cout << "\n---\nwyrazenie regularne:\n" << Y << "\n----\n"; //wupisze wyrazenie regularne
+    std::cout << "\n---\nwyrazenie regularne:\n" << regex_tekst << "\n----\n"; //wupisze wyrazenie regularne
     return 0;
 }
