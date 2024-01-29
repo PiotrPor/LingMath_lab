02105a29ef79a462fc35d4800ebe8518c63a8d8c
Piotr Porembinski <102904996+PiotrPor@users.noreply.github.com>
2024-01-29 22:00:03 +0100

    Working almost correctly
    Accepts correct sentences.
    However it accepts the incorrect sentences.

--- LL1/LL1_parser.cpp
+++ LL1/LL1_parser.cpp
@@ -1,36 +1,44 @@
-﻿#include <iostream>
+#include <iostream>
 
 #include <vector>
 #include <string>
 
-unsigned int indeks_znaku; //globalna zmienna
-
-const std::vector<char> first_C{'0','1', '2', '3', '4', '5', '6', '7', '8', '9'};
-const std::vector<char> first_O{'*', ':', '+', '-', '^'};
+//globalne zmienne
+bool czy_poprawny;
+const std::vector<char> first_C{ '0','1', '2', '3', '4', '5', '6', '7', '8', '9' };
+const std::vector<char> first_O{ '*', ':', '+', '-', '^' };
 const std::vector<char> first_P{ '0','1', '2', '3', '4', '5', '6', '7', '8', '9', ')' };
 const std::vector<char> first_W{ '0','1', '2', '3', '4', '5', '6', '7', '8', '9', ')' };
 const std::vector<char> first_S{ '0','1', '2', '3', '4', '5', '6', '7', '8', '9', ')' };
 
 bool czy_nalezy_do_pierwszych(char znak, std::vector<char> sprawdzany_wektor);
-void wywolaj_funkcje(char znak);
-void parse_C(char znak);
+//void wywolaj_funkcje(char znak);
+void parse_C(std::string ss, unsigned int numer_znaku);
+void parse_O(std::string ss, unsigned int numer_znaku);
+void parse_P(std::string ss, unsigned int* numer_znaku);
+void parse_W(std::string ss, unsigned int* numer_znaku);
+void parse_S(std::string ss, unsigned int* numer_znaku);
+void komunikat_o_zlym_znaku(unsigned int indeks_znaku, std::string ss, std::vector<char> wektor);
 
 int main()
 {
     std::string napis;
-    unsigned int a, rozmiar;
+    unsigned int indeks_znaku, rozmiar;
     std::cout << "Wpisz dzialania arytmetyczne porozdzielane srednikami (nie pisz spacji):" << std::endl;
     std::getline(std::cin, napis);
     rozmiar = napis.size();
     indeks_znaku = 0;
-    while (indeks_znaku < rozmiar)
+    czy_poprawny = true;
+    parse_S(napis, &indeks_znaku);
+    if (!czy_poprawny)
     {
-        if (indeks_znaku == 0)
-        {
-            wywolaj_funkcje(napis[0]);
-        }
+        std::cout << "dobry" << std::endl;
     }
-    return;
+    else
+    {
+        std::cout << "Niedobry" << std::endl;
+    }
+    return 0;
 }
 
 bool czy_nalezy_do_pierwszych(char znak, std::vector<char> sprawdzany_wektor)
@@ -48,11 +56,11 @@ bool czy_nalezy_do_pierwszych(char znak, std::vector<char> sprawdzany_wektor)
     return werdykt;
 }
 
-void wywolaj_funkcje(char znak)
+/*void wywolaj_funkcje(char znak)
 {
     if (czy_nalezy_do_pierwszych(znak, first_C))
     {
-        parse_C(znak);
+        //parse_C(znak);
     }
     if (czy_nalezy_do_pierwszych(znak, first_O))
     {
@@ -71,13 +79,132 @@ void wywolaj_funkcje(char znak)
         //parse_C(znak);
     }
     return;
+}*/
+
+void parse_C(std::string ss, unsigned int numer_znaku)
+{
+    char znak = ss[numer_znaku];
+    if (!czy_poprawny) { return; }
+    if (czy_nalezy_do_pierwszych(znak, first_C))
+    {
+        //nic bo to terminal?
+    }
+    else
+    {
+        czy_poprawny = false;
+    }
+    return;
 }
 
-void parse_C(char znak)
+void parse_O(std::string ss, unsigned int numer_znaku)
 {
-    if (czy_nalezy_do_pierwszych(znak, first_C)) //?
+    char znak = ss[numer_znaku];
+    if (!czy_poprawny) { return; }
+    if (czy_nalezy_do_pierwszych(znak, first_O))
     {
-        //co tu?
+        //nic bo to terminal?
     }
+    else
+    {
+        czy_poprawny = false;
+    }
+    return;
+}
+
+void parse_P(std::string ss, unsigned int* numer_znaku)
+{
+    char znak = ss[*numer_znaku];
+    //TODO
+    if (znak == '(')
+    {
+        *numer_znaku += 1;
+        parse_W(ss, numer_znaku);
+        *numer_znaku += 1;
+        znak = ss[*numer_znaku];
+        if (znak != ')')
+        {
+            czy_poprawny = false;
+            return;
+        }
+    }
+    else
+    {
+        while (czy_nalezy_do_pierwszych(ss[*numer_znaku], first_C))
+        {
+            parse_C(ss, *numer_znaku);
+            *numer_znaku += 1;
+        }
+        if (ss[*numer_znaku] == '.')
+        {
+            *numer_znaku += 1;
+            while (czy_nalezy_do_pierwszych(ss[*numer_znaku], first_C))
+            {
+                parse_C(ss, *numer_znaku);
+                *numer_znaku += 1;
+            }
+        }
+    }
+    return;
+}
+
+void parse_W(std::string ss, unsigned int* numer_znaku)
+{
+    char znak = ss[*numer_znaku];
+    if (!czy_poprawny) { return; }
+    if (czy_nalezy_do_pierwszych(znak, first_P))
+    {
+        parse_P(ss, numer_znaku);
+        while (czy_nalezy_do_pierwszych(znak, first_O))
+        {
+            parse_O(ss, *numer_znaku);
+            *numer_znaku += 1;
+            parse_P(ss, numer_znaku);
+        }
+    }
+    else
+    {
+        czy_poprawny = false;
+    }
+    return;
+}
+
+void parse_S(std::string ss, unsigned int* numer_znaku)
+{
+    char znak = ss[*numer_znaku];
+    if (czy_nalezy_do_pierwszych(znak, first_S))
+    {
+        parse_W(ss, numer_znaku);
+        unsigned int gdzie_srednik = ss.find_first_of(';', *numer_znaku);
+        if (gdzie_srednik != std::string::npos)
+        {
+            *numer_znaku = gdzie_srednik + 1;
+            parse_W(ss, numer_znaku);
+        }
+        else
+        {
+            czy_poprawny = false;
+        }
+    }
+    else 
+    {
+        czy_poprawny = false;
+    }
+    return;
+}
+
+void komunikat_o_zlym_znaku(unsigned int indeks_znaku, std::string ss, std::vector<char> wektor)
+{
+    char znak = ss[indeks_znaku];
+    std::cout << "Error! Na pozycji " << indeks_znaku << " jest \"" << znak << "\" zamiast jednego z tych: ";
+    unsigned int a;
+    for (a = 0; a < wektor.size(); a++)
+    {
+        std::cout << "\'" << wektor[a] << "\'";
+        if (a != wektor.size() - 1)
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << std::endl;
     return;
 }
